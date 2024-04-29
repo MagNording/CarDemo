3616c68d58cc81442df2b8839f5644ccb55a6e50
MagNording <magnus_nording@msn.com>
2024-04-29 11:00:17 +0200

    Some comments here and there

--- src/main/java/se/nording/bildemo/ui/CarRestController.java
+++ src/main/java/se/nording/bildemo/ui/CarRestController.java
@@ -1,9 +1,7 @@
 package se.nording.bildemo.ui;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.nording.bildemo.service.Car;
 import se.nording.bildemo.service.CarService;
 
@@ -23,6 +21,10 @@ public class CarRestController {
         Optional<Car> car = service.getCar(regNo);
         return car.map(ResponseEntity::ok).orElseGet(() ->
                 ResponseEntity.notFound().build());
+    }
 
+    @PostMapping("/rs/car")
+    public String regCar(@RequestBody Car car) {
+        return service.create(car.getRegNo(), car.getModel(), car.getYear());
     }
 }
